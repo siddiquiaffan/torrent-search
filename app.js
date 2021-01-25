c92c41e6dda035d7bd476265b7689f228ab3221b
AffanTheBest <affan207278@gmail.com>
2021-01-25 08:47:32 +0530

    Minified code,increased no of results to 10

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 window.onload = function() {
     document.querySelector('#query').focus();
 }
-function find() {
+const setHeaders = {
+    headers: {
+        Accept: 'application/json',
+    }
+}
+
+async function find() {
     let result = document.getElementById("result");
     let submit = document.querySelector('#submit');
     let loading = document.querySelector('#loading');
     let query_name = document.querySelector('#query_name');
     result.style.display = 'none';
-
     let query = document.querySelector('#query');
+
         if(query == ''){
             alert('Please enter a valid query');
         }else{
@@ -16,79 +22,46 @@ function find() {
             query.disabled = true;
             query.disabled = false;
         }
-        fetch('https://api.sumanjay.cf/torrent/?query='+query.value).then((apidata) => {
-            return apidata.json();
-        }).then((actualdata) => {
-            result.style.display = "block";
-            if(actualdata[0] == undefined){
-                result.style.display = "none";
-            }else {
-                query_name.textContent = "You searched for " + `'${query.value}'`;
-                document.querySelector('#magnet1').disabled = true;
-                document.querySelector('#title1').textContent = "Title : "+ actualdata[0].name;
-                document.querySelector('#leechers1').textContent = "Leechers : " + actualdata[0].leecher;
-                document.querySelector('#seeders1').textContent = "Seeders : " + actualdata[0].seeder;
-                document.querySelector('#size1').textContent = "Size  : " + actualdata[0].size;
-                document.querySelector('#magnet1').value = actualdata[0].magnet;
-            }
 
-            if(actualdata[1] == undefined) {
-                document.querySelector('.res2').style.display = "none"
-            }else{
-                document.querySelector('#magnet2').disabled = true;
-                document.querySelector('#title2').textContent = "Title : "+ actualdata[1].name;
-                document.querySelector('#leechers2').textContent = "Leechers : " + actualdata[1].leecher;
-                document.querySelector('#seeders2').textContent = "Seeders : " + actualdata[1].seeder;
-                document.querySelector('#size2').textContent = "Size  : " + actualdata[1].size;
-                document.querySelector('#magnet2').value = actualdata[1].magnet;
-            }
+        try{
+            var apidata = await fetch('https://api.sumanjay.cf/torrent/?query='+query.value , setHeaders);
+            var actualdata = await apidata.json();
 
-            if(actualdata[2] == undefined) {
-                document.querySelector('.res3').style.display = "none"
-            }else{
-                document.querySelector('#magnet3').disabled = true;
-                document.querySelector('#title3').textContent = "Title : "+ actualdata[2].name;
-                document.querySelector('#leechers3').textContent = "Leechers : " + actualdata[2].leecher;
-                document.querySelector('#seeders3').textContent = "Seeders : " + actualdata[2].seeder;
-                document.querySelector('#size3').textContent = "Size  : " + actualdata[2].size;
-                document.querySelector('#magnet3').value = actualdata[2].magnet;
-            }
-
-            if(actualdata[3] == undefined) {
-                document.querySelector('.res4').style.display = "none"
-            }else{
-                document.querySelector('#magnet4').disabled = true;
-                document.querySelector('#title4').innerHTML = "Title : "+ actualdata[3].name;
-                document.querySelector('#leechers4').textContent = "Leechers : " + actualdata[3].leecher;
-                document.querySelector('#seeders4').textContent = "Seeders : " + actualdata[3].seeder;
-                document.querySelector('#size4').textContent = "Size  : " + actualdata[3].size;
-                document.querySelector('#magnet4').value = actualdata[3].magnet;
-            }
-
-            if(actualdata[4] == undefined) {
-                document.querySelector('.res5').style.display = "none"
-            }else{
-                document.querySelector('#magnet5').disabled = true;
-                document.querySelector('#title5').textContent = "Title : "+ actualdata[4].name;
-                document.querySelector('#leechers5').textContent = "Leechers : " + actualdata[4].leecher;
-                document.querySelector('#seeders5').textContent = "Seeders : " + actualdata[4].seeder;
-                document.querySelector('#size5').textContent = "Size  : " + actualdata[4].size;
-                document.querySelector('#magnet5').value = actualdata[4].magnet;
-            }   
-            
+        if(actualdata[0] == undefined){
+            result.style.display = 'none';
+        }else{
+            console.log(actualdata);
+            result.style.display = 'block';
             loading.style.display = 'none';
+                for(let i=0; i < 10; i++) {
+                    var htmlData =`<div class="res${i+1}">
+                    <h5 class="text-left mt-4">Title : ${actualdata[i].name}</h5>
+                    <h5 class="text-left my-1">Leechers : ${actualdata[i].leecher}</h5>
+                    <h5 class="text-left my-1">Seeders : ${actualdata[i].seeder}</h5>
+                    <h5 class="text-left my-1">Size : ${actualdata[i].size}</h5>
+                    <h6 class="text-left mt-3">Magnet Link :</h6>
+                    <textarea class="text-left w-100 text-grey p-1 magnet" onclick="copy('magnet${i+1}')" style="color:grey!important;outline:none;" id="magnet${i+1}" readonly> ${actualdata[i].magnet}</textarea>
+                    <button class="btn btn-primary text-center mx-auto" onclick="copy('magnet${i+1}')">COPY</button>
+                    <hr class="mx-auto my-5" style="width:80%;text-align:center;background:#fff;">
+                    </div> `; 
+                    // result.insertAdjacentElement('beforeend',htmlData);
+                    result.innerHTML += htmlData;
+                }
             query.placeholder = "Enter Your query";
 
-        }).catch(function(err){
-            swal("Sorry!","Sorry , we couldn't find torrent related to your query. Please try with some other query.","error");
-            loading.style.display= "none";
-            query.value = "";
-            query.placeholder = "Please try with some other keyword";
-            query.disabled = false;
-        })
+            }
+        }catch(e){
+                swal("Sorry!","Sorry , we couldn't find torrent related to your query. Please try with some other query.","error");
+                loading.style.display= "none";
+                query.value = "";
+                query.placeholder = "Please try with some other keyword";
+                query.disabled = false;
+        }
+
         };
 
-        function copy(id){
+        // FUnuction to copy magnet to clipboard
+    function copy(id){
             let text = document.querySelector(`#${id}`); 
         navigator.clipboard.writeText(text.value).then(()=>{
             swal("Success","Magnet URL copied to to clipboard!","success");
@@ -97,6 +70,7 @@ function find() {
         });
     }
 
+    // Code for changing Theme
     var isLight = false;
 
     function toLight() {
